4cc8fafe7712b69aaa03767a1282373e828a3541
annalisape <anna.moz.pe@gmail.com>
2022-12-28 11:06:53 +0100

    update searchform and calendar

--- src/app/components/searchbar/searchbar.component.ts
+++ src/app/components/searchbar/searchbar.component.ts
@@ -1,22 +1,21 @@
 import { Component } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms'; 
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-searchbar',
   templateUrl: './searchbar.component.html',
-  styleUrls: ['./searchbar.component.scss']
+  styleUrls: ['./searchbar.component.scss'],
 })
 export class SearchbarComponent {
-
   rangeDates!: Date[];
-
-  searchForm!: FormGroup; 
+  minDate!: Date;
+  searchForm!: FormGroup;
 
   ngOnInit() {
+    this.minDate = new Date();
     this.searchForm = new FormGroup({
-      "where": new FormControl('', [Validators.required]),
-      "rangeDates": new FormControl('', [Validators.required])
-    })
+      where: new FormControl('', [Validators.required]),
+      rangeDates: new FormControl('', [Validators.required]),
+    });
   }
-
 }
