e157a064170036592a330470230ca6a3a9024c0f
annalisape <anna.moz.pe@gmail.com>
2023-01-11 17:51:37 +0100

    update markers

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -64,7 +64,44 @@ export class MapComponent implements OnInit {
             
         </div>
         <svg viewBox="-1 -1 26 32" class="hotel-marker__shape" style="fill: #003580;
-        stroke: $white;
+        stroke: white;
+        stroke-width: 1px;
+        pointer-events: none;">
+            <path d="M24 12.4286C24 19.2927 12 29 12 29C12 29 0 19.2927 0 12.4286C0 5.56446 5.37258 0 12 0C18.6274 0 24 5.56446 24 12.4286Z"></path>
+        </svg>
+    </div>
+</div>`,
+      className: '',
+      iconSize: [24, 40],
+      iconAnchor: [12, 40],
+    });
+    const svgIconBusy = L.divIcon({
+      html: `
+    <div class="hotel-marker hotel-marker--likable" style="width: 28px;
+    height: 34px;
+    position: relative;
+    pointer-events: none;">
+    <div class="hotel-marker__pin" style="width: 28px;
+    height: 34px;
+    position: absolute;
+    top: 0;
+    left: 0;
+    z-index: 2;
+    pointer-events: none;">
+        <div class="hotel-marker__icon" style="width: 8px;
+        height: 8px;
+        background-color: #d88583;
+        border: 1px solid #d88583;
+        border-radius: 50%;
+        position: absolute;
+        top: 9px;
+        left: 9px;
+        pointer-events: none;
+        z-index: 2;">
+            
+        </div>
+        <svg viewBox="-1 -1 26 32" class="hotel-marker__shape" style="fill: #003580;
+        stroke: white;
         stroke-width: 1px;
         pointer-events: none;">
             <path d="M24 12.4286C24 19.2927 12 29 12 29C12 29 0 19.2927 0 12.4286C0 5.56446 5.37258 0 12 0C18.6274 0 24 5.56446 24 12.4286Z"></path>
@@ -76,7 +113,7 @@ export class MapComponent implements OnInit {
       iconAnchor: [12, 40],
     });
 
-    const jittery = Array(5)
+    const casualHotels = Array(5)
       .fill(this.centroid)
       .map((x) => [
         x[0] + (Math.random() - 0.5) / 10,
@@ -85,7 +122,20 @@ export class MapComponent implements OnInit {
       .map((x) => L.marker(x as L.LatLngExpression, { icon: svgIcon }))
       .forEach((x) => x.addTo(this.map));
     tiles.addTo(this.map);
+
+    const casualBusyHotels = Array(5)
+    .fill(this.centroid)
+    .map((x) => [
+      x[0] + (Math.random() - 0.6) / 10,
+      x[1] + (Math.random() - 0.6) / 10,
+    ])
+    .map((x) => L.marker(x as L.LatLngExpression, { icon: svgIconBusy }))
+    .forEach((x) => x.addTo(this.map));
+
+
   }
+
+  
 
   constructor(public ref: DynamicDialogRef) {}
 
